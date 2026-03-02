500645ea6756b2d1cf8df6b2c4a17e7fdd485ff9
Mistivia <i@mistivia.com>
2026-03-03 00:47:05 +0800

    Create flake.nix

---
+++ hs-template/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Haskell project with cabal2nix and hoogle";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+        project = haskellPackages.callCabal2nix "mistivia" ./. {};
+        devTools = with haskellPackages; [
+          ghc
+          cabal-install
+          hoogle
+        ];
+
+      in
+      {
+        packages.default = project;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = devTools;
+
+          shellHook = ''
+            echo "Available commands:"
+            echo "  cabal build     - Build the project"
+            echo "  cabal test      - Run tests"
+            echo "  hoogle          - Lookup Hoogle docs"
+          '';
+        };
+      });
+}
